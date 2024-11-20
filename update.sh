7c371fe6421d26f9cfa2f5a3d85d05a7e9b0fa7f
Slushiee26 <84004366+Slushiee26@users.noreply.github.com>
2024-11-21 07:04:00 +0800

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,54 @@
+#     ██████   █████  ██████  ██   ██  █████  ██████  ███    ███ ██    ██ 
+#     ██   ██ ██   ██ ██   ██ ██  ██  ██   ██ ██   ██ ████  ████  ██  ██  
+#     ██   ██ ███████ ██████  █████   ███████ ██████  ██ ████ ██   ████   
+#     ██   ██ ██   ██ ██   ██ ██  ██  ██   ██ ██   ██ ██  ██  ██    ██    
+#     ██████  ██   ██ ██   ██ ██   ██ ██   ██ ██   ██ ██      ██    ██    
+#
+#                          ~@~ code by DANIEL HAKIM ~@~
+
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/FYPFramework/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/FYPFramework
+
+clear
+
+mkdir FYPFramework
+
+clear
+
+cd FYPFramework
+
+clear
+
+git clone https://github.com/Slushiee26/FYPFramework.git
+
+clear
+
+cd FYPFramework
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
